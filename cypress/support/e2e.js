a3cc7d85a629ccfd4bc8b70e1ccc4699d5eb8e10
Paul Dresch <paul.dresch@untereuerheim.com>
2025-06-02 22:49:19 +0200

    Testing out screenshots in reports

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,3 +1,11 @@
+import addContext from 'mochawesome/addContext';
+
 afterEach(function () {
-    cy.screenshot(this.currentTest.title, { capture: 'runner' });
-  });
+  cy.screenshot(this.currentTest.title, { capture: 'runner' });
+});
+
+Cypress.on('test:after:run', (test, runnable) => {
+  // Attach screenshot for every test
+  const screenshot = `${Cypress.config('screenshotsFolder')}/${Cypress.spec.name}/${test.title}.png`;
+  addContext({ test }, screenshot);
+});
