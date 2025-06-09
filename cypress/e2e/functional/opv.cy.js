c6c4c60095d2db8a78719e51dab50bf1099ac522
Paul Dresch <paul.dresch@untereuerheim.com>
2025-06-09 20:55:52 +0200

    Add new Summier Verstärker to opv e2e

--- cypress/e2e/functional/opv.cy.js
+++ cypress/e2e/functional/opv.cy.js
@@ -1,30 +1,56 @@
-describe('Elektronik | OPV.html', () => {
+describe('Elektronik | OPV.html (fixture-driven)', () => {
   const url = '/Elektronik/OPV.html';
+  let inputs;
+
+  before(() => {
+    cy.fixture('opvInputs').then((data) => {
+      inputs = data;
+    });
+  });
+
+  beforeEach(() => {
+    cy.visit(url);
+  });
 
   it('should calculate gain for invertierender Verstärker', () => {
-    cy.visit(url);
+    const data = inputs.invertierenderVerstaerker;
     cy.contains('button span', 'invertierender Verstärker').click();
 
-    cy.get('#R1-1').clear().type('10k');
-    cy.get('#R2-1').clear().type('100k');
+    cy.get('#R1-1').clear().type(data.R1);
+    cy.get('#R2-1').clear().type(data.R2);
     // Leave V empty to trigger gain calculation
 
     cy.get('#invertierender_Verstaerker input.Button_Berechnen').click();
 
-    cy.get('#result_invertierender_Verstaerker').should('contain', '-10.000');
+    cy.get('#result_invertierender_Verstaerker').should('contain', data.expectedGain);
   });
 
   it('should calculate output voltage for nicht-invertierender Verstärker', () => {
-    cy.visit(url);
+    const data = inputs.nichtInvertierenderVerstaerker;
     cy.contains('button span', 'nicht-invertierender Verstärker').click();
 
-    cy.get('#R1-2').clear().type('10k');
-    cy.get('#R2-2').clear().type('100k');
-    cy.get('#Uin-2').clear().type('2');
+    cy.get('#R1-2').clear().type(data.R1);
+    cy.get('#R2-2').clear().type(data.R2);
+    cy.get('#Uin-2').clear().type(data.Uin);
     // Leave Uout and V empty to trigger Uout calculation
 
     cy.get('#nicht_invertierender_Verstaerker input.Button_Berechnen').click();
 
-    cy.get('#result_nicht_invertierender_Verstaerker').should('contain', '22.000');
+    cy.get('#result_nicht_invertierender_Verstaerker').should('contain', data.expectedUout);
+  });
+
+  it('should calculate output voltage for Summier Verstärker', () => {
+    const data = inputs.summierVerstaerker;
+    cy.contains('button span', 'Summier Verstärker').click();
+
+    cy.get('#R11-3').clear().type(data.R11);
+    cy.get('#R12-3').clear().type(data.R12);
+    cy.get('#R2-3').clear().type(data.R2);
+    cy.get('#Uin1-3').clear().type(data.Uin1);
+    cy.get('#Uin2-3').clear().type(data.Uin2);
+
+    cy.get('#summier_Verstaerker input.Button_Berechnen').click();
+
+    cy.get('#result_summier_Verstaerker').should('contain', data.expectedUout);
   });
 });
